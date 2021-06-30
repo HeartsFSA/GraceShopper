d973959c61fd1567a575fe957163bbe245070872
vrlx3 <bhavsar.viral@outlook.com>
2021-06-30 11:02:17 -0500

    On L26, changed to a div to pin login component, removed {history} from incoming properties.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
 
-function Navbar({ user, setUser, history }) {
+function Navbar({ user, setUser }) {
   function handleLogout() {
     localStorage.removeItem('token')
     setUser({})
@@ -23,8 +23,7 @@ function Navbar({ user, setUser, history }) {
           </div>
         ) : (
           <div>
-            <NavLink to='/login'>Login</NavLink>
-            <NavLink to='/signup'>Sign Up</NavLink>
+            <div id='login' user={user} setUser={setUser}>Login / Register </div>
           </div>
         )}
       </nav>

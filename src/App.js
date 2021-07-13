de80c2fda76c9c7a903ee606fc56c5345e681487
Jason Van Havel <jvanhavel@gmail.com>
2021-07-13 11:29:00 -0700

    welcome message hotfix

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ function App() {
   }
 
   useEffect(() => {
-    messenger('Welcome to Wonderful World of Banana Land');
+    messenger('Welcome to Wonderful World of TicketMain!');
   }, []);
 
   return (

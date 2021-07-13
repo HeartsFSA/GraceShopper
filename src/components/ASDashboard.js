0c3c4e054c7725bc5122d7e8b11fb540e3d2c32b
Michael LaDouceur <michaelladouceur1@gmail.com>
2021-07-12 21:33:43 -0500

    adjusted accessibility to admin and seller page according to user permissions

--- src/components/ASDashboard.js
+++ src/components/ASDashboard.js
@@ -117,15 +117,16 @@ function ASDashboard(props) {
   useEffect(async () => {
     // console.log('Props: ', props)
     async function checkPermission() {
-      console.log('Checking permissions')
-      console.log('User permissions: ', user.permissions)
-      console.log('Type: ', type)
-      if((!user.id) || (user.permission < 3 && type === 'admin') || (user.permission === 2 && type === 'seller')) {
-        console.log('Pushing to login')
-        props.history.push('/')
+      if (
+        !user.id ||
+        (user.permission < 3 && type === 'admin') ||
+        (user.permission !== 2 && type === 'seller')
+      ) {
+        console.log('Pushing to login');
+        props.history.push('/');
       }
     }
-    await checkPermission()
+    await checkPermission();
     await setDashboardOrders(await getAllOrders());
     await setDashboardUsers(await getAllUsers());
   }, []);
